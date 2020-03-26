218e0eaec7c054a30095defa3e864c86bea96d6c
umairmehar <umaironline@outlook.com>
2020-03-26 15:20:39 +0500

    Added bootstrap and basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Header></Header>
     <router-view/>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {Header, Footer}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './assets/css/all.less';
 </style>
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="fixed-bottom">
+    <p>
+      CoronavirusArmy | <a href="https://www.coronavirusarmy.org">coronavirusarmy.org</a> | 2020
+    </p>
+  </div>
+</template>
+
+<script>
+  
+</script>
